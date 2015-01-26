6e74adcd8e0ad32a16c40b37c46868c468f67840
Ross Anderson <Ross-Anderson@msn.com>
2015-01-26 17:50:39 +0000

    ok

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    hi = 1
+    return HttpResponse("Rango says hey there world!")
 
 # Create your views here.
